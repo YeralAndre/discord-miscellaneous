6ce957df9ac1efe7d08263ae2e46d8e23fca60ce
Yeral_Andre <yeralreyes78@gmail.com>
2022-01-10 19:29:31 -0500

    Updated deposit funcion.

--- src/Functions/EconomySystem/Global/deposit.ts
+++ src/Functions/EconomySystem/Global/deposit.ts
@@ -7,49 +7,38 @@ export async function deposit(userID: string, amount: number | 'all') {
     userID: userID,
   });
   if (!hasUser) throw new DsMiError('This user not exists!');
-  if ((amount as number) > hasUser.balMoney) {
-    const depositNumber = (amount as number) - hasUser.balMoney;
+  if ((amount as number) > hasUser.balMoney || amount === 'all') {
+    let allAmount: number;
+    if (hasUser.balMoney > hasUser.bankCapacity - hasUser.bankMoney)
+      allAmount = hasUser.bankCapacity - hasUser.bankMoney;
+    else allAmount = hasUser.balMoney;
     await EconomySystemSchema.updateOne(
       {
         userID: userID,
       },
       {
         $inc: {
-          bankMoney: depositNumber,
-          balMoney: -depositNumber,
+          bankMoney: allAmount,
+          balMoney: -allAmount,
         },
       },
     );
-    return depositNumber as number;
+    return hasUser.balMoney as number;
   }
-  if ((amount as number) + hasUser.bankCapacity > hasUser.bankCapacity || amount === 'all') {
-    const add = hasUser.bankCapacity - hasUser.bankMoney;
-    if (add > hasUser.balMoney) {
-      await EconomySystemSchema.updateOne(
-        {
-          userID: userID,
-        },
-        {
-          $inc: {
-            bankMoney: hasUser.balMoney,
-            balMoney: -hasUser.balMoney,
-          },
-        },
-      );
-      return add;
-    } else {
-      await EconomySystemSchema.updateOne(
-        {
-          userID: userID,
-        },
-        {
-          $inc: {
-            bankMoney: add,
-            balMoney: -add,
-          },
-        },
-      );
-      return add;
-    }
-  }
+  let amountDep: number;
+  if ((amount as number) > hasUser.bankCapacity - hasUser.bankMoney)
+    amountDep = hasUser.bankCapacity - hasUser.bankMoney;
+  else amountDep = amount;
+  await EconomySystemSchema.updateOne(
+    {
+      userID: userID,
+    },
+    {
+      $inc: {
+        bankMoney: amountDep,
+        balMoney: -amountDep,
+      },
+    },
+  );
+  return amountDep as number;
 }
